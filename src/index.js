7970754f34fcbaf2d944003912d6252c02819d9e
chrissherlin <rhymeswithmerlin@gmail.com>
2018-12-18 19:38:32 -0500

    removed starter app, starting from scratch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import react and reactDOM
+import React from 'react'; // go find react directory from node modules and get all from there
+import ReactDOM from 'react-dom'; //these are ES2015, if you see require, it's a commonJS module
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+const App = () => {
+    return <div>Hello!</div>
+}
+// Take the react component and show it on the screen
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
